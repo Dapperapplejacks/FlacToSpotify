3685c1e5895c58329e987608863b382db9349516
Dapperapplejacks <danielholt456@gmail.com>
2017-07-17 12:04:07 -0400

    Cleaned up Directory Select method, added FlacReader class

--- FlacToSpot/Form1.cs
+++ FlacToSpot/Form1.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace FlacToSpot
 {
@@ -21,39 +20,24 @@ namespace FlacToSpot
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Stream dialogStream = null;
-
-            var dialog = new CommonOpenFileDialog();
-            dialog.IsFolderPicker = true;
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
-            bool fileOpen = false;
-
-            //openFileDialog.InitialDirectory = "c:\\";
-            //openFileDialog.Filter = "FLAC files (*.flac)|*.flac";
-
-
-            /*
-            DialogResult result = folderBrowserDialog.ShowDialog();
-
-            if (result == DialogResult.OK)
-            {
-                this.label1.Text = folderBrowserDialog.SelectedPath;
-            }
-             * */
-
-            CommonOpenFileDialog
-            DialogResult result = openFileDialog.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-                using (Stream stream = openFileDialog.OpenFile())
-                {
-                    
-                }
-
-                this.label1.Text = ;
-            }
-
+            this.label1.Text = GetDirectoryString();
+            
         }
+
+        private string GetDirectoryString()
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            DialogResult result = ((CommonDialog)folderBrowserDialog).ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                return folderBrowserDialog.SelectedPath;
+            }
+            else
+            {
+                //Alarm
+                return "No Directory Selected yet";
+            }
+        } 
     }
 }
