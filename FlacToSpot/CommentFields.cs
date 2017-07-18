ffecc4d445dfd16d057f23a0ff3d01229015d629
Dapperapplejacks <danielholt456@gmail.com>
2017-07-18 12:07:57 -0400

    Created CommentFields, FlacFile classes and MediaFile interface

---
+++ FlacToSpot/CommentFields.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlacToSpot
+{
+    class CommentFields
+    {
+        #region Properties
+
+        public string title
+        {
+            get
+            {
+                return title;
+            }
+            private set
+            {
+                title = value;
+            }
+        }
+        public string version
+        {
+            get
+            {
+                return version;
+            }
+            private set
+            {
+                version = value;
+            }
+        }
+        public string album
+        {
+            get
+            {
+                return album;
+            }
+            private set
+            {
+                album = value;
+            }
+        }
+        public int trackNumber
+        {
+            get
+            {
+                return trackNumber;
+            }
+            private set
+            {
+                trackNumber = value;
+            }
+        }
+        public string artist
+        {
+            get
+            {
+                return artist;
+            }
+            private set
+            {
+                artist = value;
+            }
+        }
+        public string performer
+        {
+            get
+            {
+                return performer;
+            }
+            private set
+            {
+                performer = value;
+            }
+        }
+        public string copyright
+        {
+            get
+            {
+                return copyright;
+            }
+            private set
+            {
+                copyright = value;
+            }
+        }
+        public string license
+        {
+            get
+            {
+                return license;
+            }
+            private set
+            {
+                license = value;
+            }
+        }
+        public string organization
+        {
+            get
+            {
+                return organization;
+            }
+            private set
+            {
+                organization = value;
+            }
+        }
+        public string description
+        {
+            get
+            {
+                return description;
+            }
+            private set
+            {
+                description = value;
+            }
+        }
+        public string genre
+        {
+            get
+            {
+                return genre;
+            }
+            private set
+            {
+                genre = value;
+            }
+        }
+        public string date
+        {
+            get
+            {
+                return date;
+            }
+            private set
+            {
+                date = value;
+            }
+        }
+        public string location
+        {
+            get
+            {
+                return location;
+            }
+            private set
+            {
+                location = value;
+            }
+        }
+        public string contact
+        {
+            get
+            {
+                return contact;
+            }
+            private set
+            {
+                contact = value;
+            }
+        }
+        public string ISRC
+        {
+            get
+            {
+                return ISRC;
+            }
+            private set
+            {
+                ISRC = value;
+            }
+        }
+
+        #endregion
+
+        public CommentFields(string title = "Untitled", string version = "", string album = "", int trackNumber = 0,
+                   string artist = "", string performer = "", string copyright = "", string license = "",
+                   string organization = "", string description = "", string genre = "", string date = "",
+                   string location = "", string contact = "", string ISRC = "")
+        {
+            this.title = title;
+            this.version = version;
+            this.album = album;
+            this.trackNumber = trackNumber;
+            this.artist = artist;
+            this.performer = performer;
+            this.copyright = copyright;
+            this.license = license;
+            this.organization = organization;
+            this.description = description;
+            this.genre = genre;
+            this.date = date;
+            this.location = location;
+            this.contact = contact;
+            this.ISRC = ISRC;
+        }
+
+
+
+    }
+}
